4be06224c58605e83de6ce9f82181d9e4a71f5aa
Sam Gordon <seg@well.com>
2014-10-22 00:39:01 -0700

    Problem 7: Find the 10001st prime number.

--- 07.py
+++ 07.py
@@ -1,21 +1,25 @@
 __author__ = 'segordon'
+import math
 
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
 
-
 def prime_check(n):
-    if n == 0 or n == 1:
+    if n % 2 == 0 and n > 2:
         return False
-    if n == 2 or n == 3:
-        return True
-    else:
-        for i in range(2,n-1):
-            if n % i == 0:
-                return False
-            else:
-                return True
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
 
 primes = []
+i = 1
 
 while len(primes) <= 10001:
+    if prime_check(i) == True:
+        primes.append(i)
+        i += 1
+    else:
+        i += 1
+print primes[-1]
